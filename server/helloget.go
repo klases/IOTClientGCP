fc804a4ab93d9baf0a0b171d00460ed7b37e05b9
no <no>
2019-05-31 14:14:35 +0200

    Added deploy instructions

--- server/helloget.go
+++ server/helloget.go
@@ -5,6 +5,11 @@ import (
 	"net/http"
 )
 
+/*
+To deploy run:
+gcloud functions deploy HelloGet --runtime go111 --trigger-http
+*/
+
 func HelloGet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello, World!")
 }
