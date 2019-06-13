a16bc9f1631e95ea37e864e2f7b42b54c434e7cd
no <no>
2019-06-13 16:24:59 +0200

    Log all the things

--- server/pubslurp.go
+++ server/pubslurp.go
@@ -6,14 +6,41 @@ import (
 )
 
 type PubSubMessage struct {
-	Data []byte `json:"data"`
+	Data       []byte `json:"data"`
+	Attributes *StateChangeMessage
+}
+
+type StateChangeMessage struct {
+	DeviceID  string
+	Latitude  string
+	Longitude string
+}
+
+func StateListener(ctx context.Context, m PubSubMessage) error {
+	data := string(m.Data)
+	if data != "" {
+		log.Printf("data: %s", data)
+	}
+
+	if m.Attributes == nil {
+		log.Println("No Attr")
+	} else if m.Attributes.Latitude == "" && m.Attributes.Longitude == "" {
+		log.Println("No lat or long")
+	} else if m.Attributes.Latitude == "" {
+		log.Printf("long: %s", m.Attributes.Longitude)
+	} else if m.Attributes.Longitude == "" {
+		log.Printf("lat: %s", m.Attributes.Latitude)
+	} else {
+		log.Printf("(%s, %s)", m.Attributes.Latitude, m.Attributes.Longitude)
+	}
+
+	return nil
 }
 
 /*
 To deploy run:
 gcloud functions deploy HelloPubSub --runtime go111 --trigger-topic iot_device_telematics
 */
-
 func HelloPubSub(ctx context.Context, m PubSubMessage) error {
 	name := string(m.Data)
 	if name == "" {
